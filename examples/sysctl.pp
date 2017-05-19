95e2b61432aaca8009d38e757ca26a8ea713f1c9
Andrew Brader <abrader@gmail.com>
2017-05-19 16:14:56 -0400

    Added another example to the array of hashes

--- examples/sysctl.pp
+++ examples/sysctl.pp
@@ -4,10 +4,17 @@ class { 'abrader::sysctl' :
       ensure  => 'present',
       entry   => 'net.ipv4.ip_forward',
       value   => '0',
-      #target  => '/etc/sysctl.d/forwarding.conf',
-      comment => 'Enable IP Forwarding',
-      #apply   => true,
-      #silent  => false,
+      target  => '/etc/sysctl.d/forwarding.conf',
+      comment => 'by A. Brader',
+      apply   => false
+    },
+    {
+      ensure  => 'present',
+      entry   => 'net.ipv4.tcp_synack_retries',
+      value   => '5',
+      comment => 'by S. Admin',
+      apply   => true,
+      silent  => false
     }
   ],
 }
--- types/sysctl.pp
+++ types/sysctl.pp
@@ -1,5 +1,3 @@
-# type Abrader::Sysctl = Tuple[Abrader::Sysctl::Entry, Abrader::Sysctl::Value, Abrader::Sysctl::Target, Abrader::Sysctl::Comment, Abrader::Sysctl::Apply, Abrader::Sysctl::Silent]
-
 type Abrader::Sysctl = Struct[
   {
     ensure            => Enum['present', 'absent'],
